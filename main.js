d1ff58b9ebda1e9bd80033025b43016f6e92414d
kaoula <ben.hammadi.kaoula@gmail.com>
2017-11-12 14:01:35 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var myQuote=[ { quote:"Nothing preaches better than the act! ",auteur:"Benjamin Franklin"},
+{quote:"Example is not the main thing in influencing others. It is the only thing!",auteur:"Albert Schweitzer"},
+{quote:"The road to learning by precept is long, but by example short and effective.",auteur:"Seneca"}];
+function newQuote(){
+	var item=Math.floor((Math.random() * myQuote.length));
+	return  item ;
+}
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+    	index=newQuote();
+       $('.quote').html(myQuote[index].quote);
+       $('.auteur').html(myQuote[index].auteur);
+        
+  
+     	$('#tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text='+myQuote[index].quote+' '+myQuote[index].auteur);
+   
+     	
+    
+    });
+  
+});
+

82f8374b10980ad47e51cf86848ed2f42e320dd5
kaoula <ben.hammadi.kaoula@gmail.com>
2017-11-15 13:52:02 +0100

    add readme

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var myQuote=[ { quote:"Nothing preaches better than the act! ",auteur:"Benjamin Franklin"},
 {quote:"Example is not the main thing in influencing others. It is the only thing!",auteur:"Albert Schweitzer"},
 {quote:"The road to learning by precept is long, but by example short and effective.",auteur:"Seneca"}];
+
+// choose a random quote from list myQuote
 function newQuote(){
 	var item=Math.floor((Math.random() * myQuote.length));
 	return  item ;
@@ -9,14 +11,11 @@ function newQuote(){
 
 $(document).ready(function(){
     $("button").click(function(){
+
     	index=newQuote();
        $('.quote').html(myQuote[index].quote);
-       $('.auteur').html(myQuote[index].auteur);
-        
-  
+       $('.auteur').html(myQuote[index].auteur); 
      	$('#tweetQuote').attr('href', 'https://twitter.com/intent/tweet?text='+myQuote[index].quote+' '+myQuote[index].auteur);
-   
-     	
     
     });
   
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Random Quote Machine
+In this mini-project I have developed a  web application that displays random quote, as part of my full stack JavaScript training offered by [GOMYCODE Tunisia](https://www.gomycode.tn)
+## Project brief
+This is a small web application wich does the following:
+
+<strong>User Story</strong> I can click a button to show me a new random quote.
+
+<strong>User Story</strong> I can press a button to tweet out a quote.
+
+
+
+The following screenshot shows my App:
+<p align="center">
+	<img src="appImg.png">
+</p>
+
+## What I have learned
+
+* Use the Flexible Box Layout Module to postion in the elements of my page.
+
+* Use jquery to manipulate the dom.
+
+* learn how tweet from our page .
